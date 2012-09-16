596059beef91f660753afc3cf937ce985af55b9f
Arif Widianto <sikancil.r19@gmail.com>
2012-09-16 22:33:30 +0700

    change font interface to ubuntu

---
+++ app/model/Song.js
@@ -0,0 +1,13 @@
+Ext.define('SDS.model.Song', {
+    extend: 'Ext.data.Model',
+    fields: ['id', 'name', 'artist', 'album', 'played_date', 'station'],
+    
+    proxy: {
+        type: 'ajax',
+        url: 'data/recentsongs.json',
+        reader: {
+            type: 'json',
+            root: 'results'
+        }
+    }
+});
---
+++ app/store/Stations.js
@@ -0,0 +1,5 @@
+Ext.define('SDS.store.Stations', {
+    extend: 'Ext.data.Store',
+    requires: 'SDS.model.Station',
+    model: 'SDS.model.Station'
+});
